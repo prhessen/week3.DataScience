e874d8c98d350b0dec37037e005e52a8e1336f4e
Phoebe Hessen <hesse179@umn.edu>
2020-02-05 23:14:15 -0600

    Data Analysis

--- R/week3.R
+++ R/week3.R
@@ -8,4 +8,11 @@ raw_df$timeEnd <- as.POSIXct(raw_df$timeEnd)
 clean_df <- subset(raw_df, subset = format(raw_df$timeStart,'%B')!='June')
 clean_df <- subset(clean_df, subset = q6 == 1)
 
-# Analysis
+#Analysis
+clean_df$timeSpent <- difftime(clean_df$timeEnd, clean_df$timeStart, units = "secs")
+hist(as.numeric(clean_df$timeSpent))
+frequency_tables_list <- lapply(clean_df[,5:14], table)
+lapply(frequency_tables_list, barplot)
+sum((clean_df$q1 >= clean_df$q2) & (clean_df$q2 != clean_df$q3))
+for (i in 1){
+    print(sum((clean_df$q1 >= clean_df$q2) & (clean_df$q2 != clean_df$q3)))}
